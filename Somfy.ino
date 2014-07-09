caf2de7d89d6039a68316b321e7b30dd640906f3
Tobias Joch <tobias.joch@inovex.de>
2014-07-09 21:29:24 +0200

    Add basic channel switching functionality

--- Somfy.ino
+++ Somfy.ino
@@ -3,32 +3,71 @@
  */
 
 // Remote control button 
+int myBtnPin = 3;
 int channelSelBtnPin = 4;
 
-// Timings
-int btnPressureTime = 100; // [milli seconds]
+// Home automation input commands
+int activePin = 8;
+
+boolean lastCmdActive = false;
+
+// Timings [milli seconds]
+unsigned long lastChannelSwitchTime;
+int btnPressureTime = 100;
+unsigned long minTimeBetweenChannelSwitching = 5000;
 
 /**
  * Initialize variables, pin modes, etc.
  */
 void setup() {
   Serial.begin(9600);
+  
+  // input
+  pinMode(activePin, INPUT);
+  
+  // output
+  pinMode(myBtnPin, OUTPUT);
   pinMode(channelSelBtnPin, OUTPUT);
+  
+  // timings
+  lastChannelSwitchTime = millis();
 }
 
 /**
  * Main loop
  */
 void loop() {
-  pressButton(channelSelBtnPin);
+  if (digitalRead(activePin) == HIGH &! lastCmdActive) {
+    lastCmdActive = true;
+    switchChannel(1);
+  } else if (digitalRead(activePin) == LOW) {
+    lastCmdActive = false;
+  }
+}
+
+/**
+ * Switch remote control channel.
+ * 
+ * First pressure on the button shows the actual active channel, 
+ * seconde one switch to the next channel.
+ */
+void switchChannel(int times) {
+  if (lastChannelSwitchTime + minTimeBetweenChannelSwitching < millis()) {
+    lastChannelSwitchTime = millis();
+    int var = 0; 
+    while(var < times + 1){
+      pressButton(channelSelBtnPin);    
+      var++;
+    }
+  }
 }
 
 /**
  * Simulate pressing a remote control button
  */
- void pressButton(int btn) {
-   digitalWrite(btn, HIGH);
-   delay(btnPressureTime);
-   digitalWrite(btn, LOW);
-   delay(btnPressureTime);
+ void pressButton(int btn) {   
+     digitalWrite(btn, HIGH);
+     delay(btnPressureTime);
+     digitalWrite(btn, LOW);
+     delay(btnPressureTime);
  }
