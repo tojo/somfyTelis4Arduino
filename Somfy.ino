db9a8e96a39fdff3c3b692c3d45233ea3dd787ea
Tobias Joch <tobias.joch@inovex.de>
2014-07-17 21:03:56 +0200

    First working version (switching channels, up, down and my) with commands over serial communication.

--- Somfy.ino
+++ Somfy.ino
@@ -1,22 +1,29 @@
 /*
- * Modul to controll a somfy Telis 4 RTS remote control via an Arduino.
+ * Modul to controll a Somfy Telis 4 RTS 
+ * remote control via an Arduino and serial 
+ * communication to send commands.
  */
+#include <SerialCommand.h>
 
-// Remote control button 
-int myBtnPin = 3;
-int channelSelBtnPin = 4;
-int upBtnPin = 5;
-int downBtnPin = 6;
+// Serial protocol command object
+SerialCommand SCmd;
 
-// Home automation input commands
-int activePin = 8;
+// Telis 4 remote control buttons 
+int myBtn = 3;
+int chSelBtn = 4;
+int upBtn = 5;
+int downBtn = 6;
 
-boolean lastCmdActive = false;
+// Telis 4 remote control leds
+int chOneLed = 12;
+int chFourLed = 13;
+
+int activeCh;
 
 // Timings [milli seconds]
-unsigned long lastChannelSwitchTime;
+unsigned long lastchSwitchTime;
 int btnPressureTime = 100;
-unsigned long minTimeBetweenChannelSwitching = 5000;
+unsigned long minTimeBetweenchSwitching = 5000;
 
 /**
  * Initialize variables, pin modes, etc.
@@ -25,61 +32,153 @@ void setup() {
   Serial.begin(9600);
   
   // input
-  pinMode(activePin, INPUT);
+  pinMode(chOneLed, INPUT);
+  pinMode(chFourLed, INPUT);
   
   // output
-  pinMode(myBtnPin, OUTPUT);
-  pinMode(upBtnPin, OUTPUT);
-  pinMode(downBtnPin, OUTPUT);
-  pinMode(channelSelBtnPin, OUTPUT);
+  pinMode(myBtn, OUTPUT);
+  pinMode(upBtn, OUTPUT);
+  pinMode(downBtn, OUTPUT);
+  pinMode(chSelBtn, OUTPUT);
   
   // timings
-  lastChannelSwitchTime = millis();
+  lastchSwitchTime = millis();
+
+  initActiveCh();
+
+  // Setup callbacks for SerialCommand commands 
+  SCmd.addCommand("ACTIVE",printActiveCh);
+  SCmd.addCommand("UP",cmdUp);
+  SCmd.addCommand("MY",cmdMy);
+  SCmd.addCommand("DOWN",cmdDown);
+  SCmd.addCommand("SWITCH",cmdSwitchch);
+  SCmd.setDefaultHandler(cmdUnrecognized);
+  Serial.println("Ready"); 
+}
+
+/**
+ * Initialize activeCh variable and switch the 
+ * somfy remote to the corresponding selection.
+ */
+void initActiveCh() {
+  Serial.println("initActiveCh");
+  while (!isChOneSelected()) {
+    switchch(1);
+  }
+  activeCh = 1;
 }
 
 /**
  * Main loop
  */
 void loop() {
-  if (digitalRead(activePin) == HIGH &! lastCmdActive) {
-    lastCmdActive = true;
-    //switchChannel(1);
-    pressButton(downBtnPin);
-  } else if (digitalRead(activePin) == LOW) {
-    lastCmdActive = false;
+  SCmd.readSerial(); // process serial commands
+}
+
+/**
+ * TODO: add documentation
+ */
+void cmdSwitchch() {
+  int chNumber;
+  char *ch = SCmd.next(); 
+  if (ch != NULL) {
+    chNumber=atoi(ch);
+    switchch(chNumber);
   }
 }
 
 /**
- * Switch remote control channel in the following order:
- * - channel 1
- * - channel 2
- * - channel 3
- * - channel 4
- * - all channels
- * 
- * First pressure on the button shows the actual active channel, 
- * seconde one switch to the next channel.
- *
- * int times: how many channel switching events should be emitted
+ * TODO: add documentation
  */
-void switchChannel(int times) {
-  if (lastChannelSwitchTime + minTimeBetweenChannelSwitching < millis()) {
-    lastChannelSwitchTime = millis();
+void cmdUp() {
+  cmdSwitchch();
+  pressButton(upBtn);
+}
+
+/**
+ * TODO: add documentation
+ */
+void cmdMy() {
+  cmdSwitchch();
+  pressButton(myBtn);
+}
+
+/**
+ * TODO: add documentation
+ */
+void cmdDown() {
+  cmdSwitchch();
+  pressButton(downBtn);
+}
+
+/** 
+ * This gets set as the default handler, 
+ * and gets called when no other command matches. 
+ */
+void cmdUnrecognized(const char *command) {
+  Serial.print("Valid commands: 'ACTIVE', 'UP $ch'"); 
+  Serial.println(", 'DOWN $ch', 'MY $ch', 'SWITCH $ch'"); 
+}
+
+/**
+ * Checks the selected ch of the somfy remote and
+ * returns true if the ch one is active.
+ */
+boolean isChOneSelected() {
+  pressButton(chSelBtn); // let blink the leds
+  boolean chOneInput = digitalRead(chOneLed);
+  boolean chFourInput = digitalRead(chFourLed);
+  return chOneInput && !chFourInput;
+}
+
+/**
+ * Switch remote control to the given channel.
+ *
+ * The remote supports the following channel
+ * - ch 1
+ * - ch 2
+ * - ch 3
+ * - ch 4
+ * - ch 5 -> all channel together
+ *  
+ * First pressure on the button shows the actual
+ * active channel, seconde one switch to the 
+ * next channel.
+ */
+void switchch(int ch) {
+  if (lastchSwitchTime + minTimeBetweenchSwitching < millis()) {
+    int times;
+    if (ch > activeCh) {
+      times = ch - activeCh;
+    } else {
+      // example: activeCh = 4, ch = 2 -> times must be 3
+      times = 5 - activeCh + ch;
+    }
+    lastchSwitchTime = millis();
     int var = 0; 
     while(var < times + 1){
-      pressButton(channelSelBtnPin);    
+      pressButton(chSelBtn);    
       var++;
     }
+    activeCh = ch;
+    printActiveCh();
   }
 }
 
 /**
  * Simulate pressing a remote control button
  */
- void pressButton(int btn) {   
-     digitalWrite(btn, HIGH);
-     delay(btnPressureTime);
-     digitalWrite(btn, LOW);
-     delay(btnPressureTime);
- }
+void pressButton(int btn) {   
+  digitalWrite(btn, HIGH);
+  delay(btnPressureTime);
+  digitalWrite(btn, LOW);
+  delay(btnPressureTime);
+}
+
+/**
+ * Print the active ch to Serial output for
+ * debugging purposes. 
+ */
+void printActiveCh() {
+  Serial.println(activeCh);
+}
